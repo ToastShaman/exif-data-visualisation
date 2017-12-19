b582c53418433b6e3efff7ab305e8c0b849a00f2
Kevin <kevin.denver@gmail.com>
2017-12-19 15:23:02 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# exif-data-visualisation
-A Python script using exiftool, plotly and mongodb to visualise  the exif meta data contained in your photographs.
+# Exif data visualisation
+
+A Python script using exiftool, plotly and mongodb to visualise 
+the exif meta data contained in your photographs.
+
+## Setup
+Run `$ brew install exiftool` and `pip3 install -r requirements.txt`.
+
+We also need a running mongodb instance to store the extracted meta data. 
+You can use docker to run a mongodb database.
+
+```
+$ docker run -p 27017:27017 --name metadata-mongo -d mongo
+```
+
+## Creating the Charts
+Run the script with the absolute path to the directory containing your images.
+```
+$ ./visualise_meta_data.py /Users/ked/Photography/Images
+```
---
+++ visualise_meta_data.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+import os
+import json
+from datetime import datetime
+
+from pymongo import MongoClient
+
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.graph_objs import Pie, Figure, Layout, Heatmap, Scatter
+
+
+def all_images(directory):
+    for dirpath, _, filenames in os.walk(directory):
+        for f in filenames:
+            if not f.startswith("."):
+                yield os.path.abspath(os.path.join(dirpath, f))
+
+
+def export_meta_data(image, output_format='-json'):
+    metadata = subprocess.check_output(['exiftool', output_format, image]).decode('utf-8')
+    if output_format == '-json':
+        return next(iter(json.loads(metadata)))
+    return metadata
+
+
+def import_meta_data(directory):
+    client = MongoClient('localhost', 27017)
+    exif = client.metadata.exif
+    for image in all_images(directory):
+        meta_data = export_meta_data(image)
+        exif.insert_one(meta_data)
+        print('.', end='', flush=True)
+    
+
+def focal_lengths_pie_chart():
+    def gather_data():
+        client = MongoClient('localhost', 27017)
+        exif = client.metadata.exif
+        
+        map = """
+        function focalLengthMap() {
+            if (this.FocalLength == null || this.FocalLength == 'undef') return;
+            emit(this.FocalLength, 1);
+        }"""
+
+        reduce = """
+        function focalLengthReduce(key, values) {
+            return Array.sum(values);
+        }"""
+        
+        return exif.map_reduce(map, reduce, "focal_lenght_results").find()
+
+    def create_pie_chart(results):
+        labels = []
+        values = []
+        for result in results:
+            labels.append(result['_id'])
+            values.append(result['value'])
+        plot([Pie(labels=labels, values=values)], filename='focal_lengths.html')
+    
+    create_pie_chart(gather_data())
+
+
+def time_of_day_heatmap():
+    def gather_data():
+        client = MongoClient('localhost', 27017)
+        exif = client.metadata.exif
+
+        events = [
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Monday
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Tuesday
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Wednesday
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Thursday
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Friday
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Saturday
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]  # Sunday
+        ]
+
+        for result in exif.find():
+            if result['CreateDate'] is not None:
+                parsed_date = datetime.strptime(result['CreateDate'], '%Y:%m:%d %H:%M:%S')
+                hour = parsed_date.hour
+                weekday = parsed_date.weekday()
+                events[weekday][hour] = events[weekday][hour] + 1
+        return events
+
+    def create_heatmap(events):
+        hours = ['00','01','02','03','04','05','06','07','08','09','10', '11','12','13','14','15','16','17','18','19','20','21','22','23']
+        days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday', 'Sunday']
+        data = [Heatmap(z=events, y=days, x=hours, colorscale='Viridis')]
+        layout = Layout(title='Pictures per weekday &amp; time of day', xaxis={'tickmode': 'linear'})
+        fig = Figure(data=data, layout=layout)
+        plot(fig, filename='pictures_per_weekday_and_time_of_day.html')
+
+    create_heatmap(gather_data())
+
+
+def aperture_and_shutter_speed_bubble_chart():
+    def gather_data():
+        client = MongoClient('localhost', 27017)
+        exif = client.metadata.exif
+        
+        map = """
+        function focalLengthMap() {
+            if (!this.Aperture || this.Aperture === 'undef') return;
+            if (!this.ShutterSpeed || this.ShutterSpeed === 'undef') return;
+            emit({aperture: this.Aperture, shutterSpeed: this.ShutterSpeed}, 1);
+        }"""
+
+        reduce = """
+        function focalLengthReduce(key, values) {
+            return Array.sum(values);
+        }"""
+        
+        return exif.map_reduce(map, reduce, "aperture_and_shutter_speeds").find()
+    
+    def create_bubble_chart(events):
+        shutter_speed = []
+        aperture = []
+        size = []
+        text = []
+
+        for event in events:
+            aperture.append(event['_id']['aperture'])
+            shutter_speed.append(event['_id']['shutterSpeed'])
+            size.append(event['value'])
+            text.append('Aperture: %s<br>Shutter Speed: %s<br>Size: %s' % (event['_id']['aperture'], event['_id']['shutterSpeed'], event['value']))
+
+        trace = Scatter(
+            x=shutter_speed, 
+            y=aperture, 
+            mode='markers',
+            marker={
+                'size': size
+            },
+            text=text
+        )
+
+        layout= Layout(
+            title='Aperture and Shutter Speeds', 
+            hovermode='closest',
+            xaxis={
+                'title': 'Shutter Speed',
+                'ticklen': 5,
+                'zeroline': False,
+                'gridwidth': 2
+            },
+            yaxis={
+                'range': [0, 25],
+                'title': 'Aperture',
+                'ticklen': 5,
+                'gridwidth': 2,
+                'zeroline': False
+            },
+            showlegend=False
+        )
+
+        fig = Figure(data=[trace], layout=layout)
+        plot(fig, filename='aperture_and_shutter_speed.html')
+
+    create_bubble_chart(gather_data())
+
+
+if __name__ == "__main__":
+    import_meta_data(next(iter(sys.argv[1:])))
+
+    focal_lengths_pie_chart()
+    time_of_day_heatmap()
+    aperture_and_shutter_speed_bubble_chart()
